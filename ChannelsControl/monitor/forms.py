b50eb0f39f410e129bb1829479d2ed04d74438f8
rdk72 <rdk72@rambler.ru>
2023-04-23 22:59:30 +0500

    23.04

--- ChannelsControl/monitor/forms.py
+++ ChannelsControl/monitor/forms.py
@@ -1,18 +1,27 @@
 from django import forms
 from .models import Incident
-import smart_selects.form_fields
+
+class DateTimeInput(forms.DateInput):
+    input_type = 'datetime'
+
 class IncidentEditForm(forms.ModelForm):
     class Meta:
         model = Incident
         fields =  ['provider', 'channels', 'date_time_from', 'date_time_to', 'type', 'request', 'more_info', 'state' ]
 
+        date_time_from = forms.DateTimeField(required=False, widget=DateInput(attrs={'type': 'datetime-local'}),
+                                         initial=datetime.date.today(), localize=True)
 
         widgets={
             'provider':forms.Select(attrs={'class':'form-control'}),
             #'channels':forms.SelectMultiple(attrs={'class':'chained selectfilter','data-chainfield':'provider','data-url':'/chaining/filter/monitor/Channel/provider/monitor/Incident/channels','data-auto_choose':'false', 'name':'channels','data-field-name':'Список каналов','data-value':'[2]'}),
             #'channels':smart_selects.form_fields.ChainedSelectMultiple (attrs={'class':'form-control'}),
-            'date_time_from':forms.DateTimeInput(attrs={'class':'form-control'}),
+            #'date_time_from':DateTimeInput(),
+            'date_time_to':forms.DateTimeInput(attrs={'class':'form-control'}),
+            'type':forms.Select(attrs={'class':'form-control'}),
+            'request':forms.TextInput(attrs={'class':'form-control'}),
+            'more_info':forms.Textarea(attrs={'class':'form-control','rows':3}),
+            'state':forms.Select(attrs={'class':'form-control'}),
         }
-
 
 
